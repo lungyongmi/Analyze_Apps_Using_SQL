a514b34d6d2df1e28563766e4a0178b71942c047
Wei Lung <134509885+lungyongmi@users.noreply.github.com>
2023-09-24 11:50:19 +0800

    Add files via upload

---
+++ Import_Data.sql
@@ -0,0 +1,40 @@
+-- Create Database
+CREATE DATABASE IF NOT EXISTS applestore;
+
+USE applestore;
+
+-- Create Tables and Import Data
+DROP TABLE IF EXISTS applestore;
+CREATE TABLE applestore(
+id INT NOT NULL PRIMARY KEY,
+track_name VARCHAR(255),
+size_bytes BIGINT,
+currency VARCHAR(255),
+price DECIMAL,
+rating_count_tot INT,
+rating_count_ver INT,
+user_rating DECIMAL,
+user_rating_ver DECIMAL,
+ver VARCHAR(255),
+cont_rating VARCHAR(255),
+prime_genre VARCHAR(255),
+sup_devices_num INT,
+ipadSc_urls_num INT,
+lang_num INT,
+vpp_lic INT);
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/applestore.csv'
+INTO TABLE applestore
+CHARACTER SET latin7
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+DROP TABLE IF EXISTS app_description;
+CREATE TABLE app_description(
+id INT NOT NULL,
+track_name VARCHAR(1000),
+size_bytes BIGINT,
+app_desc TEXT(50000));
